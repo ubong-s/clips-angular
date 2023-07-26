301dd3de3b0e879b294acc51ead4ccf78efc8845
Ubong Sylvester <ubongsly@gmail.com>
2023-07-26 08:39:08 +0100

    clip page updated

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  ViewEncapsulation,
+} from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import videojs from 'video.js';
+import IClip from '../models/clip.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
   styleUrls: ['./clip.component.css'],
+  providers: [DatePipe],
 })
 export class ClipComponent implements OnInit {
-  id = '';
+  @ViewChild('videoPlayer', { static: true }) target?: ElementRef;
+  player?: videojs.Player;
+  clip?: IClip;
 
   constructor(public route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params) => {
-      this.id = params['id'];
+    // this.player = videojs(this.target?.nativeElement);
+
+    this.route.data.subscribe((data) => {
+      this.clip = data['clip'] as IClip;
     });
   }
 }
